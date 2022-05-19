c3a9d4347039c7238a9043bedde5b68e7a17c285
Alfonso Ramirez <alfonso.4674@gmail.com>
2022-05-19 14:29:09 -0500

    Fix: Manage to serve and build the electron project
    The problem was with the better-sqlite3 package and it not being handled correctly in the vue.config
    More info here: https://github.com/WiseLibs/better-sqlite3/issues/488

--- server/db/helpers/db.js
+++ server/db/helpers/db.js
@@ -1,6 +1,11 @@
-const sqlite = require('better-sqlite3')
+const sqlite = require('better-sqlite3');
 const path = require('path');
-const db = new sqlite(path.resolve('public/tasks.db'), {fileMustExist: true});
+const isDevelopment = process.env.NODE_ENV !== 'production'
+const db = new sqlite(
+  isDevelopment
+  ? path.resolve('public/db/tasks.db') // if in dev mode
+  : path.join(process.resourcesPath,'db/tasks.db') // the resources path if in production build
+);
 
 function queryAll(sql, params) {
   return db.prepare(sql).all(params);
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,23 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: config => {
+    config.externals = {
+      'better-sqlite3': 'commonjs better-sqlite3'
+    }
+  },
+  pluginOptions:{
+    electronBuilder:{
+      externals: ['better-sqlite3'],
+      builderOptions:{
+        "extraResources":[
+          {
+            'from':'./public/db/',
+            'to':'db/',
+            "filter":["**/*"]
+          }
+        ]
+      }
+    }
+  }
 })
